799720fd625ebc75f37989f7f4408ffe34375f4a
PoojariAjayAnand <120207983+PoojariAjayAnand@users.noreply.github.com>
2024-02-23 20:35:57 +0530

    Delete dataloader directory

--- dataloader/dataload.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def load_dataset(path):
-    df = pd.read_csv("D:\Sentiment Analysis\hotel-reviews.csv")
-    string_to_int_map = {'happy': 1, 'not happy': 0}
-    df['Ratings'] = df['Is_Response'].map(string_to_int_map)
-    data=df[['Description','Ratings']]
-    return data
